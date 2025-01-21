f4ba4e3836416b225701d6eeb738625880c8b77e
김성수 <sskim629@gmail.com>
2025-01-21 14:11:23 +0900

    Add : "스프링 부트 프로젝트 생성"

---
+++ BE/PrePay/src/test/java/com/d111/PrePay/PrePayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d111.PrePay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrePayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
