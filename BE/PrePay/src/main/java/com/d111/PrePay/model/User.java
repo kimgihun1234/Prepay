7a5e0fc65d4251ee935776f5db4cc1ca9c09ffcb
김성수 <sskim629@gmail.com>
2025-02-03 16:11:31 +0900

    Add : 로그인, 회원가입 구현 - 김성수 2025-02-03 16:11

--- BE/PrePay/src/main/java/com/d111/PrePay/model/User.java
+++ BE/PrePay/src/main/java/com/d111/PrePay/model/User.java
@@ -1,18 +1,23 @@
 package com.d111.PrePay.model;
 
 
+import com.d111.PrePay.dto.request.UserSignUpReq;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
 
@@ -35,4 +40,11 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<OrderHistory> orderHistories = new ArrayList<>();
 
+    public User(UserSignUpReq userSignUpReq) {
+        this.userLoginId = userSignUpReq.getUserLoginId();
+        this.userName = userSignUpReq.getUserName();
+        this.email = userSignUpReq.getEmail();
+        this.userPassword = userSignUpReq.getPassword();
+        this.nickname = userSignUpReq.getNickname();
+    }
 }
--- BE/PrePay/src/main/java/com/d111/PrePay/service/UserService.java
+++ BE/PrePay/src/main/java/com/d111/PrePay/service/UserService.java
@@ -1,9 +1,35 @@
 package com.d111.PrePay.service;
 
+import com.d111.PrePay.dto.request.LoginReq;
+import com.d111.PrePay.dto.request.UserSignUpReq;
+import com.d111.PrePay.dto.respond.LoginRes;
+import com.d111.PrePay.model.User;
+import com.d111.PrePay.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public String userSignUp(UserSignUpReq userSignUpReq) {
+        User user = new User(userSignUpReq);
+        userRepository.save(user);
+        return "유저 " + user.getUserName() + "생성완료";
+    }
+
+    public LoginRes userLogin(LoginReq loginReq) throws Exception {
+        User user = userRepository.findUserByUserLoginId(loginReq.getUserLoginId());
+        if (user.getUserPassword() != loginReq.getPassword()) {
+            throw new Exception();
+        }
+        LoginRes loginRes = new LoginRes();
+        loginRes.setUserName(user.getUserName());
+        loginRes.setJwtToken("token logic");
+        return loginRes;
+    }
 }
