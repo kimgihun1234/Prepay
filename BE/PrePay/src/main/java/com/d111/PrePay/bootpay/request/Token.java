160f2582f94908888b8d668667ded8527ae75b35
김성수 <sskim629@gmail.com>
2025-02-03 15:00:56 +0900

    Add : 팀 가맹점 잔액 충전 (부트페이)  - 김성수 2025-02-03 15:00

---
+++ BE/PrePay/src/main/java/com/d111/PrePay/bootpay/request/Token.java
@@ -0,0 +1,8 @@
+package com.d111.PrePay.bootpay.request;
+
+public class Token {
+    public String application_id;
+    public String private_key;
+
+}
+
---
+++ BE/PrePay/src/main/java/com/d111/PrePay/bootpay/response/ResToken.java
@@ -0,0 +1,8 @@
+package com.d111.PrePay.bootpay.response;
+
+/**
+ * Created by ehowlsla on 2018. 5. 29..
+ */
+public class ResToken extends ResDefault {
+    public ResTokenData data;
+}
