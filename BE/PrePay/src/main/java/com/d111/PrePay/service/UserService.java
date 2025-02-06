dfb6010d399895a9a8a247f1d1ccf29f9e94e827
김성수 <sskim629@gmail.com>
2025-02-06 17:17:06 +0900

    Fix : 로그인 쿼리 수정 - 김성수 2025-02-06 17:17

--- BE/PrePay/src/main/java/com/d111/PrePay/service/UserService.java
+++ BE/PrePay/src/main/java/com/d111/PrePay/service/UserService.java
@@ -31,7 +31,7 @@ public class UserService {
 
 
     public UserLoginRes login(UserLoginReq req) {
-        User user = userRepository.findByEmailAndUserPassword(req.getUserLoginId(), req.getPassword());
+        User user = userRepository.findByEmailAndUserPasswordEquals(req.getUserLoginId(), req.getPassword());
         if (user != null) {
             UserLoginRes res = new UserLoginRes();
             res.setJwtToken(String.valueOf(user.getId()));
