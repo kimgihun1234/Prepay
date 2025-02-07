1f8e81425af6f74dd6f3eb56138f3bd8cf564f90
김성수 <sskim629@gmail.com>
2025-02-07 16:58:42 +0900

    Add : init data 기능 추가 - 김성수 2025-02-07 16:58

--- BE/PrePay/src/main/java/com/d111/PrePay/dto/request/UserSignUpReq.java
+++ BE/PrePay/src/main/java/com/d111/PrePay/dto/request/UserSignUpReq.java
@@ -1,8 +1,10 @@
 package com.d111.PrePay.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class UserSignUpReq {
     private String userName;
     private String password;
