344392cd221994e26f66907a027899557ec13cca
경이현 <wm0755123@naver.com>
2025-02-07 14:39:19 +0900

    fix : cors 테스트 - 경이현 2025-02-07 14:39

--- BE/PrePay/src/main/java/com/d111/PrePay/security/config/SecurityConfig.java
+++ BE/PrePay/src/main/java/com/d111/PrePay/security/config/SecurityConfig.java
@@ -55,22 +55,22 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.cors((cors)->
-                cors.configurationSource(new CorsConfigurationSource() {
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-
-                        CorsConfiguration configuration = new CorsConfiguration();
-
-                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-                        configuration.setAllowedMethods(Collections.singletonList("*"));
-                        configuration.setAllowCredentials(true);
-                        configuration.setAllowedHeaders(Collections.singletonList("*"));
-                        configuration.setMaxAge(3600L);
-
-                        return configuration;
-                    }
-                }));
+//        http.cors((cors)->
+//                cors.configurationSource(new CorsConfigurationSource() {
+//                    @Override
+//                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+//
+//                        CorsConfiguration configuration = new CorsConfiguration();
+//
+//                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+//                        configuration.setAllowedMethods(Collections.singletonList("*"));
+//                        configuration.setAllowCredentials(true);
+//                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+//                        configuration.setMaxAge(3600L);
+//
+//                        return configuration;
+//                    }
+//                }));
 
         http.csrf((auth) -> auth.disable());
 
