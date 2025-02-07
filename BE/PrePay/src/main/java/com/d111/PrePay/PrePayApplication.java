4bc7a399a7aba5f586532facd16283e1259897c5
김성수 <sskim629@gmail.com>
2025-02-07 12:45:52 +0900

    Fix : Test용 스웨거 옵션 해제 - 김성수 2025-02-07 12:45

--- BE/PrePay/src/main/java/com/d111/PrePay/PrePayApplication.java
+++ BE/PrePay/src/main/java/com/d111/PrePay/PrePayApplication.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(servers = {@Server(url = "https://i12d111.p.ssafy.io",description = "https"), @Server(url = "http://localhost:8080",description = "로컬")})
 @SpringBootApplication
 public class PrePayApplication {
 
