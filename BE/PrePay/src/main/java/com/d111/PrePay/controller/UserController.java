a73715a242dc2b9b20576e77099b1ffa87811b01
김성수 <sskim629@gmail.com>
2025-02-07 10:52:15 +0900

    Fix : cors 해제, 부트페이 충전메서드 수정 - 2025-02-07 10:50

--- BE/PrePay/src/main/java/com/d111/PrePay/controller/UserController.java
+++ BE/PrePay/src/main/java/com/d111/PrePay/controller/UserController.java
@@ -12,13 +12,10 @@ import com.d111.PrePay.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
