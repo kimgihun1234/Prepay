25ce0886dda1a97ff02b8142e636d7b79fb8a28a
김성수 <sskim629@gmail.com>
2025-02-08 17:22:59 +0900

    Add : 사용자 주변 2Km 이내의 가게 리스트 반환 기능 추가 - 김성수 2025-02-08 17:22

--- BE/PrePay/src/main/java/com/d111/PrePay/controller/StoreController.java
+++ BE/PrePay/src/main/java/com/d111/PrePay/controller/StoreController.java
@@ -1,11 +1,25 @@
 package com.d111.PrePay.controller;
 
+import com.d111.PrePay.dto.request.CoordinatesReq;
+import com.d111.PrePay.dto.respond.StoresRes;
+import com.d111.PrePay.service.StoreService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/store")
 @RequiredArgsConstructor
 public class StoreController {
+
+    private final StoreService storeService;
+    @PostMapping("/stores")
+    public ResponseEntity<List<StoresRes>>getNearStores(CoordinatesReq coordinatesReq) {
+        return ResponseEntity.ok(storeService.getNearStores(coordinatesReq));
+    }
 }
--- BE/PrePay/src/main/java/com/d111/PrePay/dto/respond/StoresRes.java
+++ BE/PrePay/src/main/java/com/d111/PrePay/dto/respond/StoresRes.java
@@ -1,5 +1,6 @@
 package com.d111.PrePay.dto.respond;
 
+import com.d111.PrePay.model.Store;
 import com.d111.PrePay.model.TeamStore;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,5 +18,11 @@ public class StoresRes {
         this.storeId=teamStore.getStore().getId();
         this.storeName=teamStore.getStore().getStoreName();
         this.balance= teamStore.getTeamStoreBalance();
+    }
+
+    public StoresRes(Store store) {
+        this.storeId = store.getId();
+        this.storeName=store.getStoreName();
+
     }
 }
