fdae2f96b0dbc24035f621b8200745c733b36cec
김성수 <sskim629@gmail.com>
2025-02-08 20:03:22 +0900

    Add : 개인용 qr 생성 기능 구현 김성수 2025-02-08 20:02

--- BE/PrePay/src/main/java/com/d111/PrePay/service/QrService.java
+++ BE/PrePay/src/main/java/com/d111/PrePay/service/QrService.java
@@ -2,8 +2,10 @@ package com.d111.PrePay.service;
 
 import com.d111.PrePay.dto.respond.StandardRes;
 import com.d111.PrePay.model.Qr;
+import com.d111.PrePay.model.User;
 import com.d111.PrePay.model.UserTeam;
 import com.d111.PrePay.repository.QrRepository;
+import com.d111.PrePay.repository.UserRepository;
 import com.d111.PrePay.repository.UserTeamRepository;
 import com.d111.PrePay.value.QrType;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +22,7 @@ public class QrService {
 
     private final UserTeamRepository userTeamRepository;
     private final QrRepository qrRepository;
-
+    private final UserRepository userRepository;
     public StandardRes getPartyQr(String userEmail, Long teamId) {
         Optional<UserTeam> byTeamIdAndUserEmail = userTeamRepository.findByTeamIdAndUser_Email(teamId, userEmail);
         UserTeam userTeam;
@@ -30,11 +32,18 @@ public class QrService {
             return new StandardRes("소속되지 않은 팀입니다. email or teamId 재확인", 400);
         }
         if (userTeam.isPrivilege()) {
-            Qr qr = new Qr(QrType.COMPANY_PARTY);
+            Qr qr = new Qr(QrType.COMPANY_PARTY,userTeam.getUser());
             qrRepository.save(qr);
             return new StandardRes(qr.getUuid(), 201);
         } else {
             return new StandardRes("권한이 없습니다.", 403);
         }
+    }
+
+    public StandardRes getPrivateQr(String userEmail) {
+        User user = userRepository.findUserByEmail(userEmail);
+        Qr qr = new Qr(QrType.PRIVATE, user);
+        qrRepository.save(qr);
+        return new StandardRes(qr.getUuid(),201);
     }
 }
