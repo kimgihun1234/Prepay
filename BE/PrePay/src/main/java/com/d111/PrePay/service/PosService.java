1aa23234fd907ef1b0b41aa277e23109493871f2
김성수 <sskim629@gmail.com>
2025-02-11 12:45:52 +0900

    Fix : @Transactional의 jarka, springframework 동시 의존을 해결 - 김성수 2025-02-11 12:45

--- BE/PrePay/src/main/java/com/d111/PrePay/service/PosService.java
+++ BE/PrePay/src/main/java/com/d111/PrePay/service/PosService.java
@@ -10,7 +10,6 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.util.List;
