c0cd2515afbf5c2e4101576f9c4f2a1eaeab6122
김기훈 <y2k16241@naver.com>
2025-02-11 09:50:28 +0900

    Add 개인Qr기능 구현 김기훈 2025-02-11 9:50

--- Prepay/app/src/main/java/com/example/prepay/data/remote/QrService.kt
+++ Prepay/app/src/main/java/com/example/prepay/data/remote/QrService.kt
@@ -2,13 +2,15 @@ package com.example.prepay.data.remote
 
 import com.example.prepay.data.response.QrRes
 import com.example.prepay.data.response.SignInTeamReq
-import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Header
-import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface QrService {
     //개인용 qr생성
-    @POST("/qr/private")
+    @GET("/qr/private")
     suspend fun qrPrivateCreate(@Header("userEmail") userEmail: String): QrRes
+    @GET("/qr/{teamId}/party")
+    suspend fun qrTeamCreate(@Header("userEmail") userEmail: String,@Path("teamId") teamId: Int) :QrRes
 
 }
