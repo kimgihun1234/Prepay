2740c3a6346f9ee0ad3593271c684e0986730881
DESKTOP-QFN2D55\SSAFY <y2k16241@naver.com>
2025-02-12 17:46:13 +0900

    Add 툴바로 드로어레이아웃 기능 구현 김기훈 2025-02-12 17:46

--- Prepay/app/src/main/java/com/example/prepay/data/remote/FirebaseTokenService.kt
+++ Prepay/app/src/main/java/com/example/prepay/data/remote/FirebaseTokenService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.*
 interface FirebaseTokenService {
     // Token정보 서버로 전송
     @POST("user/setFcmToken")
-    fun uploadToken(@Query("token") @Body request : TokenReq): Call<String>
+    fun uploadToken(@Header("email") email: String, @Body request : TokenReq): Call<String>
     @POST("broadcast")
     fun broadcast( @Query("title") title: String, @Query("body") body: String ): Call<String>
 
