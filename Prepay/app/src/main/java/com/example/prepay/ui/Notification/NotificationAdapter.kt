735013db6a1b5ce5a01cabd0b1d174edb4e6e64c
DESKTOP-4QCTMKG\SSAFY <okks3579@gmail.com>
2025-02-15 17:14:21 +0900

    Fix:qr 수정 -조성윤 2025-02-15 17:14

--- Prepay/app/src/main/java/com/example/prepay/ui/Notification/NotificationAdapter.kt
+++ Prepay/app/src/main/java/com/example/prepay/ui/Notification/NotificationAdapter.kt
@@ -3,6 +3,8 @@ package com.example.prepay.ui.Notification
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.prepay.R
 import com.example.prepay.data.response.OrderHistoryRes
 import com.example.prepay.databinding.ItemMyhistoryBinding
 
@@ -12,6 +14,12 @@ class NotificationAdapter(var orderHistoryList : List<OrderHistoryRes>) : Recycl
             binding.orderDate.text = orderHistoryRes.orderDate.toString()
             binding.orderAmount.text = orderHistoryRes.totalPrice.toString()
             binding.orderStoreName.text = orderHistoryRes.storeName
+            Glide.with(binding.root.context)
+                .load(orderHistoryRes.storeImgUrl)
+                // 이미지 로드중 로드 실패시에는 로고 띄워줌
+                .placeholder(R.drawable.logo)
+                .error(R.drawable.logo)
+                .into(binding.orderImage)
         }
     }
 
