be45abf482992b7101bd40f01a0c3a6cf8a0445c
김기훈 <y2k16241@naver.com>
2025-02-15 11:11:38 +0900

    FIX 버그코드 수정및 public페이지 개선 김기훈 2025-02-15 11:11

--- Prepay/app/src/main/java/com/example/prepay/ui/GroupSearch/OnPublicLikeClickListener.kt
+++ Prepay/app/src/main/java/com/example/prepay/ui/GroupSearch/OnPublicLikeClickListener.kt
@@ -1,4 +1,10 @@
 package com.example.prepay.ui.GroupSearch
 
+import com.example.prepay.data.response.LikeTeamsReq
+import com.example.prepay.data.response.PublicLikeRes
+
+
 interface OnPublicLikeClickListener {
+    fun onPublicGroupClick(publicGroupLike: PublicLikeRes)
+    fun onPublicLikeClick(publicLike: LikeTeamsReq)
 }
--- Prepay/app/src/main/java/com/example/prepay/ui/GroupSearch/PublicSearchDistanceAdapter.kt
+++ Prepay/app/src/main/java/com/example/prepay/ui/GroupSearch/PublicSearchDistanceAdapter.kt
@@ -1,43 +1,45 @@
 package com.example.prepay.ui.GroupSearch
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.prepay.R
 import com.example.prepay.data.response.LikeTeamsReq
-import com.example.prepay.data.response.PublicLikeRes
 import com.example.prepay.data.response.PublicTeamsDisRes
 import com.example.prepay.databinding.ItemPublicGroupBinding
 
-class PublicSearchLikeAdapter(var publiclikeList: List<PublicLikeRes>, private val listener: OnPublicLikeClickListener) :
-    RecyclerView.Adapter<PublicSearchLikeAdapter.PublicGroupViewHolder>() {
+class PublicSearchDistanceAdapter(var publicGroupList: List<PublicTeamsDisRes>, private val listener: OnPublicClickListener) :
+    RecyclerView.Adapter<PublicSearchDistanceAdapter.PublicGroupViewHolder>() {
 
-    class PublicGroupViewHolder(private val binding: ItemPublicGroupBinding,private val listener: OnPublicLikeClickListener) :
+    class PublicGroupViewHolder(private val binding: ItemPublicGroupBinding, private val listener: OnPublicClickListener) :
         RecyclerView.ViewHolder(binding.root) {
         private var isLiked = false  // 좋아요 상태 저장
-        fun bind(publiclike: PublicLikeRes) {
-            binding.publicName.text = publiclike.teamName
+        fun bind(publicgroup: PublicTeamsDisRes) {
+            binding.publicName.text = publicgroup.teamName
 //            binding.content.text = publicgroup.address
-//            binding.publicDistance.text = publicgroup.distance.toString()+"km"
-            binding.publicMoneyInfo.text = publiclike.balance.toString()
+            binding.publicDistance.text = publicgroup.distance.toString()+"km"
+            binding.publicMoneyInfo.text = publicgroup.teamBalance.toString()
+
             // 그룹 이미지 가지고 오기
             Glide.with(binding.root.context)
-                .load(publiclike.imageUrl)
+                .load(publicgroup.imageUrl)
                 // 이미지 로드중 로드 실패시에는 로고 띄워줌
                 .placeholder(R.drawable.logo)
                 .error(R.drawable.logo)
                 .into(binding.groupImage)
 
-            isLiked = publiclike.checkLike
+            // ❤️ 초기 하트 상태 설정 (서버 값 반영)
+            isLiked = publicgroup.like
             updateHeartIcon()
 
             // ❤️ 하트 버튼 클릭 이벤트 (서버 반영 포함)
             binding.publicSearchLikeBtn.setOnClickListener {
                 isLiked = !isLiked  // 상태 변경
                 updateHeartIcon()  // 아이콘 변경
-                val likereq = LikeTeamsReq(publiclike.teamId.toLong(), isLiked)
-                listener.onPublicLikeClick(likereq)
+                val likereq = LikeTeamsReq(publicgroup.teamId.toLong(), isLiked)
+                listener.onLikeClick(likereq)
             }
 
             // 부모 레이아웃 터치 이벤트 방지 (하트 버튼 클릭 시 아이템 클릭 방지)
@@ -46,11 +48,12 @@ class PublicSearchLikeAdapter(var publiclikeList: List<PublicLikeRes>, private v
                 false
             }
 
+            // 아이템 클릭 이벤트
             binding.root.setOnClickListener {
-                listener.onPublicGroupClick(publiclike)
+                listener.onGroupClick(publicgroup)
             }
-
         }
+
         private fun updateHeartIcon() {
             if (isLiked) {
                 binding.publicSearchLikeBtn.setImageResource(R.drawable.like_heart_fill)  // 채워진 하트 이미지
@@ -67,13 +70,13 @@ class PublicSearchLikeAdapter(var publiclikeList: List<PublicLikeRes>, private v
     }
 
     override fun onBindViewHolder(holder: PublicGroupViewHolder, position: Int) {
-        val currentGroup = publiclikeList[position]
+        val currentGroup = publicGroupList[position]
         holder.bind(currentGroup)
     }
 
 
     // 갯수 반환
     override fun getItemCount(): Int {
-        return publiclikeList.size
+        return publicGroupList.size
     }
 }
