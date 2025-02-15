f8990430ceb1ee1fcd9ba70a7f98cbf88759f5be
김기훈 <y2k16241@naver.com>
2025-02-15 15:27:04 +0900

    [FIX] 레이아웃 안 나오는 버그 수정 김기훈 2025-02-15 15:27

--- Prepay/app/src/main/java/com/example/prepay/ui/Notification/NotificationFragment.kt
+++ Prepay/app/src/main/java/com/example/prepay/ui/Notification/NotificationFragment.kt
@@ -1,14 +1,16 @@
 package com.example.prepay.ui.Notification
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.prepay.BaseFragment
 import com.example.prepay.R
 import com.example.prepay.databinding.FragmentNotificationBinding
 import com.example.prepay.ui.MainActivity
 
-
+private const val TAG = "NotificationFragment_싸피"
 class NotificationFragment : BaseFragment<FragmentNotificationBinding>(
     FragmentNotificationBinding::bind,
     R.layout.fragment_notification
@@ -24,22 +26,23 @@ class NotificationFragment : BaseFragment<FragmentNotificationBinding>(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initEvent()
         initAdapter()
+        initEvent()
     }
     private fun initEvent() {
-        TODO("Not yet implemented")
+        Log.d(TAG,"초기화중")
+        viewModel.getMyOrderHistoryList()
     }
 
     private fun initAdapter() {
 
         notificationAdapter = NotificationAdapter(arrayListOf())
         binding.rvUsageHistory.adapter = notificationAdapter
+        binding.rvUsageHistory.layoutManager = LinearLayoutManager(requireContext())
         viewModel.myOrderHistoryList.observe(viewLifecycleOwner) {
             notificationAdapter.orderHistoryList = it
             notificationAdapter.notifyDataSetChanged()
         }
-        viewModel.getMyOrderHistoryList()
     }
 
 
